ebc9b1f13a67011bd9099941b2a7a13f1cf892d7
Rachel Romee <RachelRomee@Rachels-MacBook-Pro.local>
2016-07-21 15:56:20 +0200

    Change api access for bitballoon

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 class TodoRailsapi::Application
   config.middleware.insert_before 0,  "Rack::Cors", debug: true, logger: (-> { Rails.logger }) do
     allow do
-      origins 'localhost:3000', '127.0.0.1:3000'
+      origins 'localhost:3000', '127.0.0.1:3000', 'http://recruiter-toad-65588.bitballoon.com/'
 
       resource '/cors',
         headers: :any,
